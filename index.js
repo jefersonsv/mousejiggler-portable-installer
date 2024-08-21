3b708414c4072e86bf217794e6df4bc3bd50fc42
Jeferson Tenorio <jefersonsv@gmail.com>
2024-08-21 10:27:54 +0100

    refactor lib

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import AdmZip from "adm-zip";
-import { execSync } from "child_process";
 import download from "download";
 import fs from "fs-extra";
 import os from "os";
 import path from "path";
 import format from "string-template";
+import { getGlobalPath, moveFiles, saveFile } from "./library";
 
 async function start() {
   const app = "mousejiggler";
@@ -88,56 +88,5 @@ Start-Process -FilePath "{exe}" -Wait
 
 Set-Location -Path $initialLocation
 `;
-
-function moveFiles(unzippedPath, appGlobalPath) {
-  fs.readdir(unzippedPath, (err, files) => {
-    if (err) throw err;
-
-    files.forEach((file) => {
-      const sourcePath = path.join(unzippedPath, file);
-      const destPath = path.join(appGlobalPath, file);
-
-      // Move the file
-      fs.move(sourcePath, destPath, { overwrite: true }, (err) => {
-        if (err) throw err;
-        console.log(`Moved: ${file}`);
-      });
-    });
-  });
-}
-
-function saveFile(cmdFilename, cmd) {
-  fs.writeFile(cmdFilename, cmd, (err) => {
-    if (err) {
-      console.error("Error writing to file", err);
-    } else {
-      console.log("File has been created");
-    }
-  });
-}
-
-function getGlobalPath() {
-  const userAgent = process.env.npm_config_user_agent;
-  console.log("user agent: " + userAgent);
-
-  if (userAgent) {
-    if (userAgent.includes("pnpm")) {
-      return execSync("pnpm root -g").toString().trim();
-    } else if (userAgent.includes("yarn")) {
-      return execSync("yarn global dir").toString().trim();
-    } else if (userAgent.includes("bun")) {
-      //return execSync("bun pm bin -g").toString().trim();
-      return path.join(os.homedir(), "/.bun/bin");
-    } else if (userAgent.includes("deno")) {
-      const infoJson = execSync("deno info --json").toString().trim();
-      const info = JSON.parse(infoJson);
-      return info?.config?.cache?.global;
-    } else {
-      return path.join(process.env.APPDATA, "npm");
-    }
-  } else {
-    return path.join(process.env.APPDATA, "npm");
-  }
-}
 
 start().catch((err) => console.error(err));
--- uninstall.js
+++ uninstall.js
@@ -1,10 +1,5 @@
-import AdmZip from "adm-zip";
-import { execSync } from "child_process";
-import download from "download";
-import fs from "fs-extra";
-import os from "os";
 import path from "path";
-import format from "string-template";
+import { deleteFileIfExists, getGlobalPath } from "./library";
 
 async function start() {
   const app = "mousejiggler";
@@ -18,46 +13,6 @@ async function start() {
 
   const ps1Filename = path.join(globalPath, `${app}.ps1`);
   deleteFileIfExists(ps1Filename);
-}
-
-function deleteFileIfExists(filename) {
-  if (fs.existsSync(filename)) {
-    fs.unlink(filename, (err) => {
-      if (err) {
-        console.error("Error deleting file:", err);
-        return;
-      }
-      console.log("File deleted successfully: " + filename);
-    });
-  } else {
-    console.log(
-      "The file does not exist, so it cannot be deleted: " + filename
-    );
-  }
-}
-
-function getGlobalPath() {
-  const userAgent = process.env.npm_config_user_agent;
-  console.log("user agent: " + userAgent);
-
-  if (userAgent) {
-    if (userAgent.includes("pnpm")) {
-      return execSync("pnpm root -g").toString().trim();
-    } else if (userAgent.includes("yarn")) {
-      return execSync("yarn global dir").toString().trim();
-    } else if (userAgent.includes("bun")) {
-      //return execSync("bun pm bin -g").toString().trim();
-      return path.join(os.homedir(), "/.bun/bin");
-    } else if (userAgent.includes("deno")) {
-      const infoJson = execSync("deno info --json").toString().trim();
-      const info = JSON.parse(infoJson);
-      return info?.config?.cache?.global;
-    } else {
-      return path.join(process.env.APPDATA, "npm");
-    }
-  } else {
-    return path.join(process.env.APPDATA, "npm");
-  }
 }
 
 start().catch((err) => console.error(err));
